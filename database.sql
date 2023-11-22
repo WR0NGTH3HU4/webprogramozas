6693fcaf31a68f5856569e33e081170065cd9a8e
Kulimák Máté <kulimakmate@turr.hu>
2023-11-22 09:19:31 +0100

    Adatbázis kicserélése jóra

--- database.sql
+++ database.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Nov 13. 13:09
--- Kiszolgáló verziója: 10.4.6-MariaDB
--- PHP verzió: 7.3.8
+-- Gép: localhost
+-- Létrehozás ideje: 2023. Nov 22. 09:17
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,8 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `12a_moneymanager`
+-- Adatbázis: `12A_moneymanager`
 --
+CREATE DATABASE IF NOT EXISTS `12A_moneymanager` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE `12A_moneymanager`;
 
 -- --------------------------------------------------------
 
@@ -32,10 +33,10 @@ CREATE TABLE `items` (
   `ID` int(11) NOT NULL,
   `userID` int(11) NOT NULL,
   `date` date NOT NULL,
-  `type` varchar(10) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `type` varchar(10) NOT NULL,
   `amount` int(11) NOT NULL,
-  `tag` varchar(30) COLLATE utf8mb4_hungarian_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `tag` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 -- --------------------------------------------------------
 
@@ -45,8 +46,8 @@ CREATE TABLE `items` (
 
 CREATE TABLE `tags` (
   `ID` int(11) NOT NULL,
-  `tagname` varchar(30) COLLATE utf8mb4_hungarian_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `tagname` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 -- --------------------------------------------------------
 
@@ -56,10 +57,17 @@ CREATE TABLE `tags` (
 
 CREATE TABLE `users` (
   `ID` int(11) NOT NULL,
-  `name` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `passwd` varchar(40) COLLATE utf8mb4_hungarian_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `passwd` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`ID`, `name`, `email`, `passwd`) VALUES
+(1, 'Teszt Felhasználó 1', 'teszt1@gmail.com', 'a');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -103,7 +111,7 @@ ALTER TABLE `tags`
 -- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
